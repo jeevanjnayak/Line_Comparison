c123450f2c67b6885b981c2b812665fdf535ac76
jeevan <jeevanjnayak@gmail.com>
2022-01-18 17:12:10 +0530

    updating

---
+++ LineDistance.java
@@ -0,0 +1,13 @@
+public class LineDistance {
+
+    public static void main(String[] args) {
+
+        double x1 = 1;
+        double y1 = 1;
+
+        double x2 = 5;
+        double y2 = 5;
+
+        double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+        System.out.println("Distance between two points :" + distance);
